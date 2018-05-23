e3e8dc83a013e5bcc7578b30015b8be12a48ac43
snavdeepsingh <33733091+snavdeepsingh@users.noreply.github.com>
2018-05-23 13:56:57 -0500

    Delete keys.js

--- config/keys.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    google: {
-        clientID: "55440789112-1vstnb9113fqlphg0gch30t3j4grqc0r.apps.googleusercontent.com",
-        clientSecret: "filMFf6dcFP2lqjjCnjlu32n"
-    }
-};
