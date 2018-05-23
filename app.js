3e74f38b4369995761cae8cca1175a686e9b01fc
snavdeepsingh <snavdeepsingh@gmail.com>
2018-05-23 15:32:18 -0500

    added code to connect to mongodb

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 var express = require("express");
 var authRoutes = require("./routes/auth-routes.js");
 var passportSetup = require("./config/passport-setup.js");
+var mongoose = require("mongoose");
+var keys = require("./config/keys.js");
 
 var PORT = process.env.PORT || 3000;
 var app = express();
 
 // set up view engine
 app.set("view engine", "ejs");
+
+// connect to mongodb
+mongoose.connect(keys.mongodb.dbURI, ()=> {
+    console.log("connected to mongodb");
+})
 
 // set up routes
 app.use("/auth",authRoutes);
